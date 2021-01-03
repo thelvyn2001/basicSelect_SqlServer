8cfb72b59bdbfa092aa0da8253370942c50bb3e4
thelvyn2001 <39500779+thelvyn2001@users.noreply.github.com>
2021-01-03 18:51:56 +0100

    Update selectBasic.sql

--- selectBasic.sql
+++ selectBasic.sql
@@ -1,1 +1,12 @@
 select * from dbo.Categories
+
+select * from dbo.Categories
+
+if EXISTS (select 1 from dbo.Categories where categoryName like 'Beva%')
+begin
+	select * from Categories
+end
+else
+begin
+select 'Aucune donnée' AS 'ANSWER'
+end
